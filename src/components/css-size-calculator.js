64a79bed98f5abd83da9c9e7e7d57ad135c645ec
Jack Allen <38332643+jackall3n@users.noreply.github.com>
2019-07-04 21:04:35 +0100

    Update css-size-calculator.js

--- src/components/css-size-calculator.js
+++ src/components/css-size-calculator.js
@@ -1,63 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-class Calculator extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            breakpoints: 4,
-            values: 10,
-            classes: 10,    
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: parseInt(event.target.value)
-        })
-    } 
-    
-    render() {
-        return (
-            <React.Fragment>
-                <ol>
-                    <li>
-                        <label htmlFor="breakpoints">Breakpoints:</label>
-                        <input 
-                            name="breakpoints"
-                            type="number"
-                            min="1"
-                            max="10"
-                            value={this.state.breakpoints} 
-                            onChange={this.handleChange}
-                        />
-                    </li>
-                    <li>
-                        <label htmlFor="values">Values:</label>
-                        <input 
-                            name="values"
-                            type="number"
-                            min="1"
-                            max="20"
-                            value={this.state.values} 
-                            onChange={this.handleChange}
-                        />
-                    </li>
-                    <li>
-                        <label htmlFor="classes">Classes:</label>
-                        <input 
-                            name="classes"
-                            type="number"
-                            min="1"
-                            max="20"
-                            value={this.state.classes} 
-                            onChange={this.handleChange}
-                        />
-                    </li>
-                </ol>
-                <h4>Total CSS classes: {this.state.values*this.state.classes*this.state.breakpoints}</h4>
+const INITIAL_STATE = {
+  breakpoints: 4,
+  values: 10,
+  classes: 10,
+};
 
-            </React.Fragment>
-            )
-    }
-}
-export default Calculator
+const Calculator = () => {
+
+  const [state, setState] = useState(INITIAL_STATE);
+  const { breakpoints, values, classes } = state;
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    setState({
+      breakpoints,
+      values,
+      classes,
+      [name]: value,
+    });
+  };
+
+  const onReset = () => {
+    setState(INITIAL_STATE);
+  };
+
+  return (
+    <>
+      <ol>
+        <li>
+          <label htmlFor="breakpoints">Breakpoints:</label>
+          <input
+            name="breakpoints"
+            type="number"
+            min="1"
+            max="10"
+            value={breakpoints}
+            onChange={handleChange}
+          />
+        </li>
+        <li>
+          <label htmlFor="values">Values:</label>
+          <input
+            name="values"
+            type="number"
+            min="1"
+            max="20"
+            value={values}
+            onChange={handleChange}
+          />
+        </li>
+        <li>
+          <label htmlFor="classes">Classes:</label>
+          <input
+            name="classes"
+            type="number"
+            min="1"
+            max="20"
+            value={classes}
+            onChange={handleChange}
+          />
+        </li>
+      </ol>
+      <h4>Total CSS classes: {values * classes * breakpoints}</h4>
+      <button onClick={onReset}>Reset</button>
+    </>
+  );
+};
